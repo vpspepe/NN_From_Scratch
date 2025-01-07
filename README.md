f5537ccdf05c841316bea29740da7416b98f3d8a
= <victor101pepe@hotmail.com>
2025-01-07 10:06:39 -0300

    feat: update README + use argparser on nano_gpt to configure it

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - Part 2: Makemore, now using embeddings, learning rate decay and minibatches. Also used batch normalization and verify how the paramaters distribution flow through the NN.
 - Part 3: Study the backpropagation process, calculating each gradient by hand, since the loss function until the embeddings and checking if it matches PyTorch gradients. No features were added to Makemore.
 - Part 4: Makemore, now using a more complex model (WaveNet architecture). Also the different layers were modularized in classes. Use of flatten layers
+- NanoGPT Notebook: Used only to study the self-attention mechanism, such as Key, Query and Value concepts. Not pretty functional
+- NanoGPT script: Full implementation of the Char-Level GPT, with the transformers architecture. It was also used skip connections, dropout layers and layer-normalization. Usefull classes/functions are inside the `gpt` folder. It can reuse pre-trained models to sample and generate some text (it was originally trained on Shakespeare books) or to train new ones. The hyperparameters of the new model should be setted in a `.json` file, and the input text in a `.txt` file (look at `bigram_params.json` and `input.txt`, as they are the default files used when the script is runned). You can also use different files/models by passing CLI args:
+- - `--model_file`: must be a `.pth` file with your pre-trained model. If passed, will only generate some new text for fun :)
+- - `--text_file`: must be a `.txt` file with the text you want to train (useless if `--model_file` is given). If not given, default to `input.txt`
+-- `hyperparams_file`: must be a `.json` with the exactly same template as `bigrams.json` (useless if `--model_file` is given). If not given, default to `bigram_params.json`
--- nano_gpt.py
+++ nano_gpt.py
@@ -7,15 +7,24 @@ import torch.nn.functional as F
 from gpt.bigram import BigramModel
 from gpt.funcs import train_test_val_split, get_batch, estimate_loss
 import datetime as dt
+import argparse
+torch.manual_seed(42)
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Reuse or train new model")
+
+# Add arguments
+parser.add_argument("--model_file", type=str, help="Model file to reuse", required=False)
+parser.add_argument("--text_file", type=str, help="Input text file to reuse", 
+                    required=False, default="input.txt")
+parser.add_argument("--hyperparams_file", type=str, help="Json file with hyperparams", 
+                    required=False, default="bigram_params.json")
+
+# Parse arguments
+args = parser.parse_args()
 
 # Input file: all Shakespeare books
-text = open("input.txt", 'r', encoding='utf-8').read()
-
-# Hyperparams
-hyperparams = json.loads(open("bigram_params.json", 'r').read())
-
-# Seed
-torch.manual_seed(42)
+text = open(args.text_file, 'r', encoding='utf-8').read()
 
 # Vocabulary and encode/decode
 chars = sorted(list(set(text)))
@@ -27,46 +36,60 @@ itos = {v:k for k,v in stoi.items()}
 encode = lambda x: [stoi[ch] for ch in x]
 decode = lambda x: ''.join([itos[i] for i in x])
 
-# Train Test Val Split
-data = torch.tensor(encode(text), dtype=torch.long)
-train_data, val_data, test_data = train_test_val_split(data, 
-                                                       0.8, 
-                                                       0.2, 
-                                                       0)
-
-# Model
-model = BigramModel(vocab_size=vocab_size,
-                    block_size=hyperparams["block_size"],
-                    n_embd=hyperparams["n_embd"],
-                    n_heads=hyperparams["n_head"],
-                    n_layers=hyperparams["n_layer"],
-                    dropout=hyperparams["dropout"]
-                    )
+# Device (GPU if available)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
-# Optimizing it
 
-optim = torch.optim.AdamW(model.parameters(), lr=hyperparams["learning_rate"])
+# If we are reusing a model
+if args.model_file:
+    # Load the model
+    model = torch.load(args.model_file)
+    model = model.to(device)
+    
+else:
+    # Hyperparams
+    hyperparams = json.loads(open(args.hyperparams_file, 'r').read())
 
-for iter in range(hyperparams["max_iters"]):
+    # Train Test Val Split
+    data = torch.tensor(encode(text), dtype=torch.long)
+    train_data, val_data, test_data = train_test_val_split(data, 
+                                                        0.8, 
+                                                        0.2, 
+                                                        0)
 
-    # Sometimes do eval
-    if iter % hyperparams["eval_interval"] == 0:
-        losses = estimate_loss(model, hyperparams["eval_iters"], train_data, val_data, hyperparams["block_size"], hyperparams["batch_size"], device)
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    # Model
+    model = BigramModel(vocab_size=vocab_size,
+                        block_size=hyperparams["block_size"],
+                        n_embd=hyperparams["n_embd"],
+                        n_heads=hyperparams["n_head"],
+                        n_layers=hyperparams["n_layer"],
+                        dropout=hyperparams["dropout"]
+                        )
+    model = model.to(device)
 
-    # Take batch
-    xb, yb = get_batch('train', train_data, val_data, 
-                       hyperparams["block_size"], hyperparams["batch_size"], device)
+    # Optimizing it
+    optim = torch.optim.AdamW(model.parameters(), lr=hyperparams["learning_rate"])
 
-    # Measure the loss
-    logits, loss = model(xb, yb)
-    optim.zero_grad(set_to_none=True)
-    loss.backward()
-    optim.step()
+    for iter in range(hyperparams["max_iters"]):
 
-model_name = f"bigram_model_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.pth"
-torch.save(model.state_dict(), model_name)
+        # Sometimes do eval
+        if iter % hyperparams["eval_interval"] == 0:
+            losses = estimate_loss(model, hyperparams["eval_iters"], train_data, val_data, hyperparams["block_size"], hyperparams["batch_size"], device)
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        # Take batch
+        xb, yb = get_batch('train', train_data, val_data, 
+                        hyperparams["block_size"], hyperparams["batch_size"], device)
+
+        # Measure the loss
+        logits, loss = model(xb, yb)
+        optim.zero_grad(set_to_none=True)
+        loss.backward()
+        optim.step()
+
+    model_name = f"bigram_model_"
+    model_name += "_".join([f"{k}_{v}" for k,v in hyperparams.items()])
+    model_name += f"_{dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.pth"
+    torch.save(model, model_name)
 
 # Sample from the model now
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
